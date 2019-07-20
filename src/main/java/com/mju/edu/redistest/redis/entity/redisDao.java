0780c2ef4b82c19652e7bdabb5aadf125037aab8
lwg <1447311377@qq.coom>
2019-07-20 20:59:41 +0800

    修改规范

--- src/main/java/com/mju/edu/redistest/redis/entity/redisDao.java
+++ src/main/java/com/mju/edu/redistest/redis/entity/redisDao.java
@@ -3,9 +3,21 @@ package com.mju.edu.redistest.redis.entity;
 import org.apache.ibatis.annotations.Param;
 import java.util.List;
 import com.mju.edu.redistest.redis.entity.redis;
-
+/**
+ *功能描述
+ * @author Mr.Lin
+ * @date 2019/7/20
+ * @param
+ * @return
+ */
 public interface redisDao {
-
+    /**
+     *功能描述 dsfdf
+     * @author Mr.Lin
+     * @date 2019/7/20
+     * @param [pojo]
+     * @return int
+     */
     int insert(@Param("pojo") redis pojo);
 
     int insertList(@Param("pojo") List< redis> pojo);
